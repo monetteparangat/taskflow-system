1db6280053490a6d661ee74d442f1d9af22aa17d
MonetteParangat <monette.parangat@ibm.com>
2024-11-28 07:29:51 +0800

    initial comit

---
+++ src/main/java/com/example/taskflow_system/TaskflowSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskflow_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskflow_system/TaskflowSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskflow_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
