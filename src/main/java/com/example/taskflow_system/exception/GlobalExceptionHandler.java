8b3c95db06ba9d148a1a649b9143e1d73c512426
MonetteParangat <monette.parangat@ibm.com>
2024-12-03 17:23:18 +0800

    Comit new exceptionhandlers

--- src/main/java/com/example/taskflow_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskflow_system/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.example.taskflow_system.exception;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,5 +27,42 @@ public class GlobalExceptionHandler {
 		
 		return ResponseEntity.badRequest().body(new ApiResponse<>(errorMessage, false));
 	}
+	
+	@ExceptionHandler(TaskNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> handleTaskNotFoundException(TaskNotFoundException e){
+		ApiResponse<String> response = new ApiResponse<String>(e.getMessage(), false);
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(WorkflowNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> handleWorkflowNotFoundException(WorkflowNotFoundException e){
+		ApiResponse<String> response = new ApiResponse<>(e.getMessage(), false);
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<ApiResponse<?>> handleUserNotFoundException(UserNotFoundException e){
+		ApiResponse<String> response = new ApiResponse<String>(e.getMessage(), false);
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse<?>> handleValidateException(MethodArgumentNotValidException e){
+		StringBuilder errorMessage = new StringBuilder();
+		e.getBindingResult().getFieldErrors().forEach(error -> 
+		errorMessage
+		.append(error.getField())
+		.append(": ")
+		.append(error.getDefaultMessage())
+		.append("; "));
+		return null;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiResponse<?>> handleGlobalException(Exception e){
+		ApiResponse<String> response = new ApiResponse<>(e.getMessage(), false);
+		return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
 	
 }
