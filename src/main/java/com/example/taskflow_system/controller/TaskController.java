74a85efa699ad3760a75c73b207ca7d3addd04df
MonetteParangat <monette.parangat@ibm.com>
2024-12-09 21:13:27 +0800

    Implement Logger on Controller and Service

--- src/main/java/com/example/taskflow_system/controller/TaskController.java
+++ src/main/java/com/example/taskflow_system/controller/TaskController.java
@@ -1,5 +1,7 @@
 package com.example.taskflow_system.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -28,64 +30,83 @@ import jakarta.validation.constraints.NotNull;
 @Validated
 public class TaskController {
 
+	private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 	@Autowired
 	private TaskService taskService;
 
-	@GetMapping("/get")
-	public ResponseEntity<ApiResponse<TaskDTO>> getTaskById(@RequestParam(value = "id") @NotNull Long id) {
-		ApiResponse<TaskDTO> taskDTO = taskService.getTaskById(id);
-		return new ResponseEntity<>(taskDTO, HttpStatus.OK);
-	}
-
 	@GetMapping("/getAll")
 	public ResponseEntity<ApiResponse<Page<TaskDTO>>> getAllTasks(Pageable pageable) {
+		logger.info("Start getAllTasks at controller: Fetching all tasks");
 		ApiResponse<Page<TaskDTO>> tasksDTO = taskService.getAllTasks(pageable);
+		logger.info("End getAllTasks at controller: Successfully fetched all tasks");
 		return new ResponseEntity<>(tasksDTO, HttpStatus.OK);
+	}
+
+	@GetMapping("/get")
+	public ResponseEntity<ApiResponse<TaskDTO>> getTaskById(@RequestParam(value = "id") @NotNull Long id) {
+		logger.info("Start getTaskByID at controller: Fetching task with ID: {}", id);
+		ApiResponse<TaskDTO> taskDTO = taskService.getTaskById(id);
+		logger.info("End getTaskByID at controller: Successfully fetched task with ID: {}", id);
+		return new ResponseEntity<>(taskDTO, HttpStatus.OK);
 	}
 
 	@PostMapping("/create")
 	public ResponseEntity<ApiResponse<TaskDTO>> createTask(@RequestBody TaskDTO taskDTO) {
+		logger.info("Start createTask at controller");
 		ApiResponse<TaskDTO> createdTask = taskService.createTask(taskDTO);
+		logger.info("End createTask at controller");
 		return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/update")
 	public ResponseEntity<ApiResponse<TaskDTO>> updateTask(@RequestParam(value = "id") @NotNull Long id,
 			@RequestBody TaskDTO taskDTO) {
+		logger.info("Start updateTask at controller: Updating task with ID: {}", id);
 		ApiResponse<TaskDTO> updatedTask = taskService.updateTask(id, taskDTO);
+		logger.info("End updateTask at controller: Updated task with ID: {}", id);
 		return new ResponseEntity<>(updatedTask, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete")
 	public ResponseEntity<ApiResponse<Void>> deleteTask(@RequestParam(value = "id") @NotNull Long id) {
+		logger.info("Start deleteTask at controller: Deleting task with ID: {}", id);
 		ApiResponse<Void> response = taskService.deleteTaskById(id);
+		logger.info("End deleteTask at controller: Deleted task with ID: {}", id);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
 	@GetMapping("/get/by-status")
 	public ResponseEntity<ApiResponse<Page<TaskDTO>>> getTaskByStatus(
 			@RequestParam(value = "status") @NotNull Status status, Pageable pageable) {
+		logger.info("Start getTaskByStatus at controller: Fetch task with status: {}", status);
 		ApiResponse<Page<TaskDTO>> taskDTO = taskService.getTasksByStatus(status, pageable);
+		logger.info("End getTaskByStatus at controller: Fetch task with status: {}", status);
 		return new ResponseEntity<>(taskDTO, HttpStatus.OK);
 	}
 
 	@GetMapping("/get/by-user")
 	public ResponseEntity<ApiResponse<Page<TaskDTO>>> getTasksByUser(@RequestParam(value = "id") @NotNull Long id,
 			Pageable pageable) {
+		logger.info("Start getTasksByUser at controller: Fetching task with user ID: {}", id);
 		ApiResponse<Page<TaskDTO>> taskDTO = taskService.getTasksByUser(id, pageable);
+		logger.info("End getTasksByUser at controller: Fetching task with user ID: {}", id);
 		return new ResponseEntity<>(taskDTO, HttpStatus.OK);
 	}
 
 	public ResponseEntity<ApiResponse<Page<TaskDTO>>> getTasksByWorkflow(@RequestParam(value = "id") @NotNull Long id,
 			Pageable pageable) {
+		logger.info("Start getTasksByWorkflow at controller: Fetching task with workflow ID: {}", id);
 		ApiResponse<Page<TaskDTO>> taskDTO = taskService.getTasksByWorkflow(id, pageable);
+		logger.info("End getTasksByWorkflow at controller: Fetching task with workflow ID: {}", id);
 		return new ResponseEntity<>(taskDTO, HttpStatus.OK);
 	}
 
 	@PatchMapping("/update/status")
 	public ResponseEntity<ApiResponse<Void>> updateStatus(@RequestParam(value = "status") @NotNull Status status,
 			@RequestParam(value = "id") @NotNull Long id) {
+		logger.info("Start updateStatus at controller: Update task status with ID: {}", id);
 		ApiResponse<Void> response = taskService.changeStatus(id, status);
+		logger.info("End updateStatus at controller: Update task status with ID: {}", id);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
--- src/main/java/com/example/taskflow_system/service/TaskServiceImpl.java
+++ src/main/java/com/example/taskflow_system/service/TaskServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.taskflow_system.service;
 
+import org.aspectj.weaver.bcel.AtAjAttributes;
+import org.hibernate.service.spi.Startable;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +31,7 @@ import java.util.stream.Collectors;
 @Service
 public class TaskServiceImpl implements TaskService {
 
+	private static final Logger logger = LoggerFactory.getLogger(TaskServiceImpl.class);
 	@Autowired
 	private TaskRepository taskRepository;
 	@Autowired
@@ -38,21 +43,26 @@ public class TaskServiceImpl implements TaskService {
 
 	@Override
 	public ApiResponse<Page<TaskDTO>> getAllTasks(Pageable pageable) {
+		logger.info("Start getAllTasks at Service");
 		Page<Task> tasks = taskRepository.findAll(pageable);
 		Page<TaskDTO> tasksDTO = tasks.map(task -> mapper.map(task, TaskDTO.class));
+		logger.info("End getAllTasks at Service");
 		return new ApiResponse<>(Constant.TASK_RETRIEVED_SUCCESS, true, tasksDTO);
 	}
 
 	@Override
 	public ApiResponse<TaskDTO> getTaskById(Long id) {
+		logger.info("Start getTaskById at Service: Fetching task with ID: {}", id);
 		Task task = taskRepository.findById(id)
 				.orElseThrow(() -> new TaskNotFoundException(String.format(Constant.TASK_NOT_FOUND, id)));
 		TaskDTO taskDTO = mapper.map(task, TaskDTO.class);
+		logger.info("End getTaskById at Service");
 		return new ApiResponse<>(Constant.TASK_RETRIEVED_SUCCESS, true, taskDTO);
 	}
 
 	@Override
 	public ApiResponse<TaskDTO> createTask(TaskDTO taskDTO) {
+		logger.info("Start createTask at Service");
 		Task task = mapper.map(taskDTO, Task.class);
 
 		Long taskId = task.getId();
@@ -63,11 +73,13 @@ public class TaskServiceImpl implements TaskService {
 		Task saveTask = taskRepository.save(task);
 
 		TaskDTO savedTaskDTO = mapper.map(saveTask, TaskDTO.class);
+		logger.info("End createTask at Service");
 		return new ApiResponse<>(Constant.TASK_SAVED_SUCCESS, true, savedTaskDTO);
 	}
 
 	@Override
 	public ApiResponse<TaskDTO> updateTask(Long id, TaskDTO taskDTO) {
+		logger.info("Start updateTask at Service");
 		TaskDTO updatedTaskDTO = null;
 		Task existingTask = taskRepository.findById(id)
 				.orElseThrow(() -> new TaskNotFoundException(String.format(Constant.TASK_NOT_FOUND, id)));
@@ -102,41 +114,49 @@ public class TaskServiceImpl implements TaskService {
 			return new ApiResponse<>(String.format(Constant.TASK_UPDATED_FAILED, id), false, updatedTaskDTO);
 		}
 
+		logger.info("End updateTask at Service");
 		return new ApiResponse<>(String.format(Constant.TASK_UPDATED_SUCCESS, id), true, updatedTaskDTO);
 	}
 
 	@Override
 	public ApiResponse<Void> deleteTaskById(Long id) {
+		logger.info("Start deleteTaskById at Service");
 		Boolean isExists = taskRepository.existsById(id);
 		if (!isExists) {
 			return new ApiResponse<>(String.format(Constant.TASK_NOT_FOUND, id), false);
 		}
 		taskRepository.deleteById(id);
+		logger.info("End deleteTaskById at Service");
 		return new ApiResponse<>(String.format(Constant.TASK_DELETED_SUCCESS, id), true);
 
 	}
 
 	public ApiResponse<Void> changeStatus(Long id, Status status) {
+		logger.info("Start changeStatus at Service");
 		Task task = taskRepository.findById(id)
 				.orElseThrow(() -> new TaskNotFoundException(String.format(Constant.TASK_NOT_FOUND, id)));
 		task.setStatus(status);
 		setStartDateIfInProgress(task);
 		setEndDateIfCompleted(task);
 		taskRepository.save(task);
+		logger.info("End changeStatus at Service");
 		return new ApiResponse<>(String.format(Constant.TASK_STATUS_UPDATE_SUCCESS, status), true);
 	}
 
 	public ApiResponse<Page<TaskDTO>> getTasksByUser(Long userId, Pageable pageable) {
+		logger.info("Start getTasksByUser at Service");
 		Page<Task> tasksPage = taskRepository.findByUserId(userId, pageable);
 
 		if (tasksPage.isEmpty()) {
 			return new ApiResponse<>(String.format(Constant.USERS_TASKS_NOT_FOUND, userId), false, Page.empty());
 		}
 		Page<TaskDTO> tasksPageDTO = tasksPage.map(task -> mapper.map(task, TaskDTO.class));
+		logger.info("End getTasksByUser at Service");
 		return new ApiResponse<>(String.format(Constant.USERS_TASKS_RETRIEVED, userId), true, tasksPageDTO);
 	}
 
 	public ApiResponse<Page<TaskDTO>> getTasksByWorkflow(Long workflowId, Pageable pageable) {
+		logger.info("Start getTasksByWorkflow at Service");
 		Page<Task> tasksPage = taskRepository.findByWorkflowId(workflowId, pageable);
 
 		if (tasksPage.isEmpty()) {
@@ -145,10 +165,12 @@ public class TaskServiceImpl implements TaskService {
 		}
 
 		Page<TaskDTO> tasksPageDTO = tasksPage.map(task -> mapper.map(task, TaskDTO.class));
+		logger.info("End getTasksByWorkflow at Service");
 		return new ApiResponse<>(String.format(Constant.WORKFLOWS_TASKS_RETRIEVED, workflowId), true, tasksPageDTO);
 	}
 
 	public ApiResponse<Page<TaskDTO>> getTasksByStatus(Status status, Pageable pageable) {
+		logger.info("Start getTasksByStatus at Service");
 		Page<Task> tasksPage = taskRepository.findByStatus(status, pageable);
 
 		if (tasksPage.isEmpty()) {
@@ -156,21 +178,26 @@ public class TaskServiceImpl implements TaskService {
 		}
 
 		Page<TaskDTO> tasksPageDTO = tasksPage.map(task -> mapper.map(task, TaskDTO.class));
+		logger.info("End getTasksByStatus at Service");
 		return new ApiResponse<>(String.format(Constant.STATUS_TASKS_RETRIEVED, status), true, tasksPageDTO);
 	}
 
 	public void setStartDateIfInProgress(Task task) {
+		logger.info("Start setStartDateIfInProgress at Service");
 		if (task.getStatus().equals(Status.IN_PROGRESS) && task.getStartDate() == null) {
 			task.setStartDate(LocalDateTime.now());
 			taskRepository.save(task);
 		}
+		logger.info("End setStartDateIfInProgress at Service");
 	}
 
 	public void setEndDateIfCompleted(Task task) {
+		logger.info("Start setEndDateIfCompleted at Service");
 		if (task.getStatus().equals(Status.COMPLETED) && task.getEndDate() == null) {
 			task.setEndDate(LocalDateTime.now());
 			taskRepository.save(task);
 		}
+		logger.info("End setEndDateIfCompleted at Service");
 	}
 
 }
